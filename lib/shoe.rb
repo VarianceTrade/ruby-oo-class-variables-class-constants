d366c4c9641d4f6c1fd3675e0bcbfb69318da8ed
Brandon <sugb73@gmail.com>
2021-02-09 11:09:01 -0500

    commit tree 28e0644caa5af6ca59fca906ce67d263554c488c

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  # I removed this line of code - attr_reader :brand
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+
+    if !BRANDS.include? brand
+      BRANDS.push(brand)
+    end
   end
 
   def cobble
